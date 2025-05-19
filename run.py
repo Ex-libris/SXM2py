38fe8a0cab2db27d74b8bd9fdd2828a5a1e8bedd
Ex-libris <etellion@gmail.com>
2025-05-19 09:21:57 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ def main():
         folder_path=r"Q:\USERS\BEN\Scripts\20250512",
         output_subfolder="Processed",
         keywords=["TopoFwd", "It_extFwd"]
+    )
     export_images_to_pptx(
         image_folder=r"Q:\USERS\BEN\Scripts\20250512\Processed",
         output_pptx_path=r"Q:\USERS\BEN\Scripts\20250512\STM_Figures.pptx",
         images_per_slide=4
-    )
     )
 
 if __name__ == "__main__":
